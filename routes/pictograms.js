bec0bd178d367cf4e77256fcd65b42d7629201fd
kim.jihye.dev <kim.jihye.dev@gmail.com>
2020-02-05 04:49:38 +0900

    N:M테이블 처리 완료

--- routes/pictograms.js
+++ routes/pictograms.js
@@ -7,36 +7,53 @@ const Op = models.Sequelize.Op;
 const Pictogram = models.Pictogram;
 
 // 모든 픽토그램의 정보 읽어오기
-router.get('/',async(req,res)=>{
-    try{
+router.get('/', async (req, res) => {
+    try {
         let result = await Pictogram.findAll({
-            order:[['pictogram_name','ASC']]
+            order: [['pictogram_name', 'ASC']]
         });
         res.json(result); // 리턴값은 배열(json들어있는 배열)
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 });
 
 // 픽토그램 등록
-router.post('/',async(req,res)=>{
+// findOrCreate 
+// 중복값이 있을 경우 내부적으로 처리
+// 없을 경우에는 새로운 객체를 생성한다.
+router.post('/', async (req, res) => {
     // async,await의 반환값은 Promise 객체
-    try{
+    try {
         console.log('요청옴')
-        // pictogram_type: req.body.pictogram_type,
-        let result = await Pictogram.create({
-            pictogram_name: req.body.pictogram_name,
-            image_path: req.body.image_path
+        let result = await Pictogram.findOrCreate({
+            where: { pictogram_name: req.body.pictogram_name },
+            defaults: {
+                pictogram_name: req.body.pictogram_name,
+                image_path: req.body.image_path
+            }
         })
-        // image_path: req.body[0].image_path
-        console.log(req.body);
-        console.log('결과는?')
-        console.log(result);
-        res.status(201).json({
-            message:'등록성공'
-        });
+            // findOrCreate에서는 리턴값이 2개이므로 then을 사용X
+            // spread 를 사용한다.
+            .spread((pictogram, created) => {
 
-    }catch(err){
+                if (created) { // 새로 작성한 경우 created 가 반환
+                    res.json({
+                        code: 201,
+                        message: '픽토그램 신규 등록 성공'
+                    });
+                    console.log('신규작성');
+                    console.log(created); // 리턴값은 true
+                }else { // 기존에 존재하는 경우 
+                    res.json({
+                        code: 409,
+                        mesage: '이미 등록된 픽토그램입니다. '
+                    });
+                    console.log('중복');
+                    console.log(pictogram); // 리턴값은 DB정보 객체
+                }
+            })
+    } catch (err) {
         console.log(err);
     }
 });
@@ -45,8 +62,8 @@ router.post('/',async(req,res)=>{
 
 // 특정 픽토그램의 정보 읽어오기
 // 파라미터가 있는 경우에는 라우터 최하단에 작성해야 한다.
-router.get('/:id',async (req,res)=>{
-    
+router.get('/:id', async (req, res) => {
+
 })
 
 
--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,11 @@ const router = express.Router();
 // 시퀄라이즈 모듈 가져오기
 const models = require('../models/index');
 const Sequelize = models.Sequelize;
+const sequelize = models.sequelize;
 const Op = models.Sequelize.Op;
 const Product = models.Product;
 const Ingredient = models.Ingredient;
-const Pictograms = models.Pictogram; // N:M 관계를 맺은 테이블
+const Pictogram = models.Pictogram; // N:M 관계를 맺은 테이블
 // 주의할 점은 새로 생긴 관계 테이블(product_pictograms)이 아니라
 // 관계를 맺은 테이블을 사용한다는 점
 
@@ -26,46 +27,85 @@ router.get('/', async (req, res, next) => {
 });
 
 router.post('/',async(req,res,next)=>{
-      // 3개의 테이블에 데이터를 입력하고 그 결과를 받아온다.
-      // 그 결과들을 어떻게 묶지? 배열? 객체? -> 객체로 해보자
-    let returnObj  = {
-        productObj:{},
-        pictogramObj:{},
-        ingredientObj:{}
-    }
 
     try{
         console.log('리퀘스트객체'+req);
         console.log("리퀘스트바디"+req.body);
-        let result = await Product.create({
-            parent_category: req.body.parent_category,
-            child_category:req.body.child_category,
-            product_name:req.body.product_name,
-            product_image:req.body.product_image, 
-            product_desc:req.body.product_desc, 
-            suggested_use:req.body.suggested_use, 
-            warnings:req.body.warnings, 
-            servings:req.body.servings, 
-            total_servings:req.body.total_servings, 
-            other_ingredients:req.body.other_ingredients, 
-        });
+        let result = await Product.findOrCreate({
+            where:{product_name:req.body.product_name},
+            defaults: {
+                parent_category: req.body.parent_category,
+                child_category:req.body.child_category,
+                product_name:req.body.product_name,
+                product_image:req.body.product_image, 
+                product_desc:req.body.product_desc, 
+                suggested_use:req.body.suggested_use, 
+                warnings:req.body.warnings, 
+                servings:req.body.servings, 
+                total_servings:req.body.total_servings, 
+                other_ingredients:req.body.other_ingredients, 
+            }
+        })  // async를 선언한 함수가 끝났으므로 다시 async선언
+        .spread(async (product,created) => {
+            let productId;
+            let pictogramId= [];
+            if(created){
+                let pictogram = req.body.pictogram;
+                console.log(`픽토그램확인`)
+                console.log(pictogram); // 결과는 내가 보낸 배열
+                for(idx in pictogram ){
+                    if(pictogram[idx] === ''){
+                        continue;
+                    }else{
+                        var result = await Pictogram.findOne({
+                            where:{ 'pictogram_name': pictogram[idx]}
+                        })
+                    }
+                    console.log('결과출력')
+                    console.log(result);
+                    console.log(`id얻어옴`)
+                    pictogramId.push(result.dataValues.id); // 픽토그램 id를 배열에 넣는다.
+                    console.log(`내가 원하는거`);
+                    console.log(pictogramId);
+                }
+                 // 다음은 위에서 작성한 상품의 id를 알아오자
+                    result = await Product.findOne({
+                        where:{'product_name':req.body.product_name}
+                    })
+                    console.log('결과출력')
+                    console.log(result);
+                    productId = result.dataValues.id;
+                    console.log(`내가 원하는거`);
+                    console.log(productId);
 
-        console.log(result); // 리턴값은 객체로 각종 DB정보가 담겨져 온다
-       
-        res.status(200).json({
-            message:'등록성공'
+                    // id 둘 다 알아왔으므로 product_pictograms에 등록하기
+                    for(idx in pictogramId){
+                        sequelize.query(`INSERT INTO product_pictograms values(now(),now(),${productId},${pictogramId[idx]})`)
+                        .then((results, metadata) => {
+                            console.log(results) // result는 [ 0, 1 ]이 반환되는데 무슨 의미인지 모르겠음
+                        })
+                    }
+                    // 다음은 성분테이블에 정보를 넣자
+                    // ingredientsArr로 배열 타입이다
+                    // productId 가 foreign key
+
+            }else {
+                res.json({
+                    code:409,
+                    message:'이미 등록된 상품입니다.'
+                })
+            }
         })
-         // returnObj.productObj = result; // 리턴할 객체에 추가
-        // // 반복문 돌려야 할 듯
-        // // req.body.pictogram -> 배열
-        // let pictograms = req.body.pictogram;
-        // for(idx in pictograms ){
-        //     result = await Pictogram.findOne({
-        //         where:{ 'pictogram_name':pictogram[idx]}
-        //     })
-        // }
-        // console.log('여기서부터가 중요')
-        // console.log(result); 
+        
+        
+         
+        // 반복문 돌려야 할 듯
+        // req.body.pictogram -> 배열
+        
+
+        // res.status(200).json({
+        //     message:'등록성공'
+        // })
 
     } catch(err){
         console.log(err);
