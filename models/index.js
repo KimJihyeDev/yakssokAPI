df7c264e22bf0f8a0cecede0aaaf82b0f2eebeb8
kim.jihye.dev <kim.jihye.dev@gmail.com>
2020-03-10 15:40:11 +0900

    production모드 테스트

--- models/index.js
+++ models/index.js
@@ -14,19 +14,29 @@ const db = {};
 // const sequelize = new Sequelize(config.database,config.username,config.password,config);
 // 비밀번호 노출을 막기위해 비밀번호를 환경변수로 분리하여 객체 생성
 
-if (env === 'development') {
-    const sequelize = new Sequelize(
-        config.database,
-        config.username,
-        process.env.DB_PWD, config,
-    )
-} else {
-    const sequelize = new Sequelize(
-        config.database,
-        config.username,
-        process.env.DB_PRODUCTION, config,
-    )
-}
+let password;
+    env == 'development'
+        ? password = process.env.DB_PWD
+        : password = process.env.DB_PRODUCTION
+
+var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    password, config,
+)
+// if (env == 'development') {
+//     var sequelize = new Sequelize(
+//         config.database,
+//         config.username,
+//         process.env.DB_PWD, config,
+//     )
+// } else {
+//     var sequelize = new Sequelize(
+//         config.database,
+//         config.username,
+//         process.env.DB_PRODUCTION, config,
+//     )
+// }
 
 // DB객체에 시퀄라이즈 객체를 속성에 바인딩한다.
 db.sequelize = sequelize;
